bb4fbc523de4dea9fa7227e9b8a456dbfce340df
TannoValk1 <158517011+TannoValk1@users.noreply.github.com>
2024-09-19 10:49:45 +0300

    Instructor Edit

--- ContosoUniversity/obj/Release/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Release/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a94e3d1c532b039f4f5d87b6a230b54d56124c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
