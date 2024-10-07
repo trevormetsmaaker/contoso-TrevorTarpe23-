d2cb9ebdad2aedb1b870bca6c481f86aa5da2087
trevormetsmaaker <158562002+trevormetsmaaker@users.noreply.github.com>
2024-10-07 15:29:09 +0300

    Add files via upload

---
+++ SchoolContext.cs
@@ -0,0 +1,40 @@
+﻿
+using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Instructor> Instructors { get; set; }
+
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Instructor>().ToTable("Instructors");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignments");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignments");
+            modelBuilder.Entity<Department>().ToTable("Departments");
+            //modelBuilder.Entity<AssignedCourseData>().ToTable("AssignedCourseData");
+        }
+    }
+
+
+}
