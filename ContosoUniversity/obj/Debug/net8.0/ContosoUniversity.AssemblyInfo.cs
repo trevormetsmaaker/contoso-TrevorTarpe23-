83dc2592ef5f466cd9131b8dc76a60f1a82ccbd1
trevormetsmaaker <158562002+trevormetsmaaker@users.noreply.github.com>
2025-01-31 10:24:06 +0200

    suva
    ö

--- ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cb9ebdad2aedb1b870bca6c481f86aa5da2087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e88f0b00416506da851992087b7ec0b3223862a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
